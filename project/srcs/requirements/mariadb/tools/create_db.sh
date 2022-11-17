42789f259d718bd35dc3531e99628bcb0a549396
Mariya SOSNOVA <msosnova@e3r9p13.clusters.42paris.fr>
2022-11-17 16:37:03 +0100

    17.11.2022

--- project/srcs/requirements/mariadb/tools/create_db.sh
+++ project/srcs/requirements/mariadb/tools/create_db.sh
@@ -1,13 +1,13 @@
 service mysql start 
 
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MDB_DATABASE};" \
+mysql -e "CREATE DATABASE IF NOT EXISTS $MDB_DATABASE;" \
 
-mysql -e "CREATE USER IF NOT EXISTS '${MDB_ADMIN}'@'%' IDENTIFIED by '${MDB_MDP}';" \
+mysql -e "CREATE USER IF NOT EXISTS '$MDB_USER'@'%' IDENTIFIED by '$MDB_PASS';" \
 
-mysql -e "GRANT ALL PRIVILEGES ON ${MDB_DATABASE}.* TO '${MDB_ADMIN}'@'%';" \
+mysql -e "GRANT ALL PRIVILEGES ON $MDB_DATABASE.* TO '$MDB_USER'@'%';" \
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '${MDB_ROOT_MDP}';" \
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '$MDB_ROOT_PASS';" \
 
-mysql -u root -p${MDB_ROOT_MDP} -e "FLUSH PRIVILEGES;"
+mysql -u root -p$MDB_ROOT_PASS -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+mysqladmin -u root -p$MDB_ROOT_PASS shutdown
