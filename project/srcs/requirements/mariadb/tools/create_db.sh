944cd1160daa2348a9a2a70fb3061ca0c1496fa5
Mariya SOSNOVA <msosnova@e3r7p18.clusters.42paris.fr>
2022-11-29 17:01:42 +0100

    WP

--- project/srcs/requirements/mariadb/tools/create_db.sh
+++ project/srcs/requirements/mariadb/tools/create_db.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 #echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';\nCREATE DATABASE IF NOT EXISTS $MDB_DATABASE;\nCREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$MDB_PASS';\nGRANT ALL PRIVILEGES ON $MDB_DATABASE.* TO $MDB_USER@'%';\n" > init.sql;
 
@@ -7,16 +7,20 @@
 
 
 service mysql start 
-/etc/init.d/mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $MDB_DATABASE;" \
+#/etc/init.d/mysql start
 
-mysql -e "CREATE USER IF NOT EXISTS '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASS';" \
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`" \
 
-mysql -e "GRANT ALL PRIVILEGES ON $MDB_DATABASE.* TO '$MDB_USER'@'%';" \
+mysql -e "CREATE USER IF NOT EXISTS \'${MDB_USER}'@'%' IDENTIFIED BY '${MDB_PASS}';" \
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '$MDB_ROOT_PASS';" \
+mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \'${MDB_USER}\'@'%';" \
 
-mysql -u root -p$MDB_ROOT_PASS -e "FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '${MDB_ROOT_PASS}';" \
+
+mysql -e "FLUSH PRIVILEGES;"
+#mysql -u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$MDB_ROOT_PASS shutdown
+
+exec mysqld_safe
--- project/srcs/requirements/wordpress/conf/wp-config.sh
+++ project/srcs/requirements/wordpress/conf/wp-config.sh
@@ -1,15 +1,3 @@
-# #!/bin/bash
-
-# wp core download --allow-root --path=/var/www/
-
-# wp config create --skip-check --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbhost=$DB_HOST --dbpass=$DB_USER_PASS --path=/var/www
-
-# wp core install --allow-root --url=$DOMAIN_NAME --title=inception --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=$WORDPRESS_ADMIN_EMAIL --path=/var/www
-
-# wp user create --allow-root $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASS --path=/var/www
-
-# exec "$@"
-
 #!/bin/bash
 
 sleep 5
@@ -34,3 +22,6 @@ wp theme install twentyseventeen --activate --allow-root
 wp cache flush --allow-root
 
 mkdir /run/php
+
+exec /usr/sbin/php-fpm7.3 -F -R
+
