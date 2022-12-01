c1ac81233855cdb6510c45b62b00c138074cc26c
Mariya SOSNOVA <msosnova@e3r9p13.clusters.42paris.fr>
2022-12-01 14:19:22 +0100

    all -test

--- project/srcs/requirements/mariadb/tools/create_db.sh
+++ project/srcs/requirements/mariadb/tools/create_db.sh
@@ -1,26 +1,14 @@
-#!/bin/bash
-
-#echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';\nCREATE DATABASE IF NOT EXISTS $MDB_DATABASE;\nCREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$MDB_PASS';\nGRANT ALL PRIVILEGES ON $MDB_DATABASE.* TO $MDB_USER@'%';\n" > init.sql;
-
-#exec "$@"
-
-
-
-service mysql start 
-
-#/etc/init.d/mysql start
-
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`" \
-
-mysql -e "CREATE USER IF NOT EXISTS \'${MDB_USER}'@'%' IDENTIFIED BY '${MDB_PASS}';" \
-
-mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \'${MDB_USER}\'@'%';" \
-
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '${MDB_ROOT_PASS}';" \
-
-mysql -e "FLUSH PRIVILEGES;"
-#mysql -u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
-
+service mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MDB_USER}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \`${MDB_USER}\`@'%'IDENTIFIED BY '${MDB_PASS}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '${MDB_ROOT_PASS}';"
+mysql     -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$MDB_ROOT_PASS shutdown
 
-exec mysqld_safe
+#/etc/init.d/mysql start
+# mysql-u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
+
+#!/bin/bash
+#echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';\nCREATE DATABASE IF NOT EXISTS $MDB_DATABASE;\nCREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$MDB_PASS';\nGRANT ALL PRIVILEGES ON $MDB_DATABASE.* TO $MDB_USER@'%';\n" > init.sql;
+#exec "$@"
