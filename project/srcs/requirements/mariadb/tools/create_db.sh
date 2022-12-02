4b353e17f838b57a6d66b2673ba504a83f9c5e0a
msosnova <msosnova@student.42.fr>
2022-12-02 07:32:34 +0100

    wp ne march pas

--- project/srcs/requirements/mariadb/tools/create_db.sh
+++ project/srcs/requirements/mariadb/tools/create_db.sh
@@ -1,13 +1,12 @@
 service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MDB_USER}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \`${MDB_USER}\`@'%'IDENTIFIED BY '${MDB_PASS}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${MDB_ADMIN}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \`${MDB_ADMIN}\`@'%'IDENTIFIED BY '${MDB_PASS}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '${MDB_ROOT_PASS}';"
-mysql     -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$MDB_ROOT_PASS shutdown
+mysql-u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
 
 #/etc/init.d/mysql start
-# mysql-u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
+# 
 
 #!/bin/bash
 #echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';\nCREATE DATABASE IF NOT EXISTS $MDB_DATABASE;\nCREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$MDB_PASS';\nGRANT ALL PRIVILEGES ON $MDB_DATABASE.* TO $MDB_USER@'%';\n" > init.sql;
--- project/srcs/requirements/wordpress/conf/wp-config.sh
+++ project/srcs/requirements/wordpress/conf/wp-config.sh
@@ -11,12 +11,12 @@ wp core download --allow-root --locale=fr_FR
 
 if [ ! -f /var/www/html/wordpress/wp-config.php ];
 then
-	wp config create --allow-root --dbname=${MDB_DATABASE} --dbuser=${MDB_ADMIN} --dbpass=${MDB_MDP} --dbhost=${MDB_HOST_PORT} --path=${WP_WEBSITE_PATH}
+	wp config create --allow-root --dbname=${MDB_DATABASE} --dbuser=${MDB_ADMIN} --dbpass=${MDB_PASS} --dbhost=${MDB_HOST_PORT} --path=${WP_WEBSITE_PATH}
 fi
 
 wp core install --allow-root --url=${WP_DOMAIN_NAME} --title=${WP_WEBSITE_NAME} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_MDP} --admin_email=${WP_ADMIN_MAIL}
 
-wp user create --allow-root ${WP_NUSER} ${WP_NUSER_MAIL} --role=author --user_pass=${WP_NUSER_MDP}
+wp user create --allow-root ${WP_USER} ${WP_USER_MAIL} --role=author --user_pass=${WP_USER_PASS}
 
 wp theme install twentyseventeen --activate --allow-root
 wp cache flush --allow-root
