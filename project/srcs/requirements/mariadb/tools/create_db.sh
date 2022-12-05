072e6cec40c6473ce2e6b97dd922a5920533305a
msosnova <msosnova@student.42.fr>
2022-12-05 06:42:07 +0100

    last

--- project/srcs/requirements/mariadb/tools/create_db.sh
+++ project/srcs/requirements/mariadb/tools/create_db.sh
@@ -1,11 +1,12 @@
-service mysql start;
+/bin/sh -c service mysql start;
+#/etc/init.d/mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MDB_ADMIN}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \`${MDB_ADMIN}\`@'%'IDENTIFIED BY '${MDB_PASS}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '${MDB_ROOT_PASS}';"
 mysql-u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
 
-#/etc/init.d/mysql start
+
 # 
 
 #!/bin/bash
