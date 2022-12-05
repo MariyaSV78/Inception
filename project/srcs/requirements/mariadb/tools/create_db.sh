c2a0ffb0f63774dd2a4221a7c608343701fba72d
msosnova <msosnova@student.42.fr>
2022-12-05 23:57:47 +0100

    last05.12.2022

--- project/srcs/requirements/mariadb/tools/create_db.sh
+++ project/srcs/requirements/mariadb/tools/create_db.sh
@@ -1,10 +1,18 @@
-#!/bin/bash service mysql start;
+#!/bin/bash
+
+service mysql start
+
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld 
+chown -R mysql:mysql /var/run/mysqld/mysqld.sock 
+chown -R mysql:mysql /var/run/mysqld/mysqld.pid
+chmod -R 644 /var/run/mysqld/mysqld.sock
+chmod -R 644 /etc/mysql/mariadb.conf.d/mdb.cnf
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MDB_ADMIN}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \`${MDB_ADMIN}\`@'%'IDENTIFIED BY '${MDB_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \`${MDB_ADMIN}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '${MDB_ROOT_PASS}';"
-mysql-u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
+mysql -u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
 
 
 # 
