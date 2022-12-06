a376507086943fa358a90630c152e1ddfad7a1b8
msosnova <msosnova@student.42.fr>
2022-12-06 07:42:40 +0100

    last06.12.2022

--- project/srcs/requirements/mariadb/tools/create_db.sh
+++ project/srcs/requirements/mariadb/tools/create_db.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-service mysql start
+# MDB_DATABASE=wordpress
+# MDB_ROOT_PASS=rootpass
+# MDB_ADMIN=msosnova
+# MDB_PASS=pass
 
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld 
-chown -R mysql:mysql /var/run/mysqld/mysqld.sock 
-chown -R mysql:mysql /var/run/mysqld/mysqld.pid
-chmod -R 644 /var/run/mysqld/mysqld.sock
+# chown -R mysql:mysql /var/run/mysqld/mysqld.sock 
+# chown -R mysql:mysql /var/run/mysqld/mysqld.pid
+# chmod -R 644 /var/run/mysqld/mysqld.sock
 chmod -R 644 /etc/mysql/mariadb.conf.d/mdb.cnf
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MDB_ADMIN}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \`${MDB_ADMIN}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '${MDB_ROOT_PASS}';"
-mysql -u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
+service mysql start
 
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MDB_ROOT_PASS}';\
+CREATE DATABASE IF NOT EXISTS ${MDB_DATABASE};\
+CREATE USER IF NOT EXISTS '${MDB_ADMIN}'@'%' IDENTIFIED BY '${MDB_PASS}';\
+GRANT ALL PRIVILEGES ON ${MDB_DATABASE}.* TO '${MDB_ADMIN}'@'%';\
+flush privileges;"
 
 # 
 
 #!/bin/bash
-#echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';\nCREATE DATABASE IF NOT EXISTS $MDB_DATABASE;\nCREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$MDB_PASS';\nGRANT ALL PRIVILEGES ON $MDB_DATABASE.* TO $MDB_USER@'%';\n" > init.sql;
+#echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MDB_ROOT_PASS';\nCREATE DATABASE IF NOT EXISTS $MDB_DATABASE;\nCREATE USER IF NOT EXISTS $MDB_ADMIN@'%' IDENTIFIED BY '$MDB_PASS';\nGRANT ALL PRIVILEGES ON $MDB_DATABASE.* TO $MDB_ADMIN@'%';\n" > init.sql;
 #exec "$@"
 
-#/bin/sh -c service mysql start;
+
+
+
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${MDB_DATABASE}\`;"
+# mysql -e "CREATE USER IF NOT EXISTS \`${MDB_ADMIN}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${MDB_DATABASE}\`.* TO \`${MDB_ADMIN}\`@'%' IDENTIFIED BY '${MDB_PASS}';"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED by '${MDB_ROOT_PASS}';"
+# mysql -u root -p${MDB_ROOT_PASS} -e "FLUSH PRIVILEGES;"
