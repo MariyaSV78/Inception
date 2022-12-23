b224e9427764a087bbd74374fc3deb10ad7253d1
msosnova <msosnova@42.student.fr>
2022-12-23 14:31:04 +0100

    last

--- inc/srcs/requirements/mariadb/create_mdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';\nCREATE DATABASE $DB_NAME;\nCREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_USER_PASS';\nGRANT ALL ON $DB_NAME.* TO $DB_USER@'%';\n" > init.sql;
-
-exec "$@"
--- project/srcs/requirements/wordpress/conf/wp-config.sh
+++ project/srcs/requirements/wordpress/conf/wp-config.sh
@@ -2,19 +2,19 @@
 
 sleep 5
 
-mkdir -p /var/www/html/wordpress
+mkdir -p /var/www/html
 chmod 777 /var/www/html
 chown -R www-data:www-data /var/www/html/
-cd /var/www/html/wordpress/
+cd /var/www/html
 
 wp core download --allow-root --locale=fr_FR
 
-if [ ! -f /var/www/html/wordpress/wp-config.php ];
+if [ ! -f /var/www/html/wp-config.php ];
 then
 	wp config create --allow-root --dbname=${MDB_DATABASE} --dbuser=${MDB_ADMIN} --dbpass=${MDB_PASS} --dbhost=${MDB_HOST_PORT} --path=${WP_WEBSITE_PATH}
 fi
 
-wp core install --allow-root --url=${WP_DOMAIN_NAME} --title=${WP_WEBSITE_NAME} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_MDP} --admin_email=${WP_ADMIN_MAIL}
+wp core install --allow-root --url=${WP_DOMAIN_NAME} --title=${WP_WEBSITE_NAME} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_MAIL}
 
 wp user create --allow-root ${WP_USER} ${WP_USER_MAIL} --role=author --user_pass=${WP_USER_PASS}
 
